da3bb4b0ba5c765a6a04279078ea3a15ea608b41
SashaPlavi <sasha.bingulac@gmail.com>
2020-12-08 10:16:06 +0100

    trigerin custom events

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -130,6 +130,7 @@ var User =
 function () {
   function User(data) {
     this.data = data;
+    this.events = {};
   }
 
   User.prototype.get = function (propName) {
@@ -138,6 +139,25 @@ function () {
 
   User.prototype.set = function (updete) {
     Object.assign(this.data, updete);
+  };
+
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
+
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(function (callback) {
+      //console.log('bla');
+      return callback();
+    });
   };
 
   return User;
@@ -157,8 +177,19 @@ var user = new User_1.User({
   name: 'sasa',
   age: 38
 });
-user.set({});
-console.log(user.get('name'));
+user.on('change', function () {
+  console.log('change1');
+});
+user.on('change', function () {
+  console.log('change2');
+});
+user.on('click', function () {
+  console.log('click');
+});
+user.on('hover', function () {
+  console.log('hover');
+});
+user.trigger('dasdasda');
 },{"./models/User":"src/models/User.ts"}],"../../../../AppData/Roaming/nvm/v10.17.0/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -187,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56391" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60768" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@ import { User } from './models/User';
 
 const user = new User({ name: 'sasa', age: 38 });
 
-user.set({});
+user.on('change', () => {
+  console.log('change1');
+});
+user.on('change', () => {
+  console.log('change2');
+});
+user.on('click', () => {
+  console.log('click');
+});
+user.on('hover', () => {
+  console.log('hover');
+});
 
-console.log(user.get('name'));
+user.trigger('dasdasda');
