41143b2503bf75d3334998ceb7f2dc94e24efb7c
SashaPlavi <sasha.bingulac@gmail.com>
2020-12-19 14:27:16 +0100

    fly

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
-import { UserEdit } from './view/UserEdit';
-import { User } from './models/User';
+import { UserList } from './view/UserList';
+import { Collections } from './models/Collections';
+import { User, UserProps } from './models/User';
 
-const user = User.builUser({ name: 'Perica', age: 20 });
+const users = new Collections(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.builUser(json);
+  }
+);
 
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
 
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('root element not found');
-}
+users.fetch();
+
+// import { UserEdit } from './view/UserEdit';
+// import { User } from './models/User';
+
+// const user = User.builUser({ name: 'Perica', age: 20 });
+
+// const root = document.getElementById('root');
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+
+//   userEdit.render();
+//   console.log(userEdit);
+// } else {
+//   throw new Error('root element not found');
+// }
